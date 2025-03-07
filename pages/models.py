c445dfe577f91fe42b359c518376c6a2202795ad
Munira <144519028+MuniraGulova@users.noreply.github.com>
2025-03-07 12:40:49 +0500

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-from pages.analysis_page import X, y, df_removed
+from pages.analysis_page import X
+from pages.analysis_page import y
+from pages.analysis_page import df_removed 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, TargetEncoder, OneHotEncoder
 from sklearn.preprocessing import StandardScaler
