6b3785716dc44d246df8bb2c3a8caa2b7013f8bf
Munira <144519028+MuniraGulova@users.noreply.github.com>
2025-03-07 13:17:59 +0500

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pages.analysis_page import X, y, df_copy
+from pages.analysis_page import df_removed, X, y 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, TargetEncoder, OneHotEncoder
 from sklearn.preprocessing import StandardScaler
@@ -14,7 +14,7 @@ from navigation import make_sidebar
 
 make_sidebar()
 
-df_removed = df_copy.iloc[:5] 
+
 st.title("🧠 Workplace Mental Health Survey")
 
 st.write("Explore how workplace factors and personal demographics relate to mental health at work.")
